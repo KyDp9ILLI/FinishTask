8d52d9608c62c54aced65706ac2d2628c9307610
KyDp9ILLI <a-chikilev@mail.ru>
2022-08-24 07:24:00 +0300

    push

---
+++ task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+Console.Write("Введите размер массива:");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+
+string[] CreateArray (int size) 
+{
+    string[] arr = new string[size];
+    for (int i = 0; i<size; i++)
+    {
+        Console.Write("Введите элемент массива:");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+string[] FormatArray(string[] arr) {
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3){
+            count++;
+        }
+    }
+    string[] newArr = new string[count];
+    int a = 0;
+    for (int i = 0;  i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) {
+            newArr[a] = arr[i];
+            a++;
+        }
+    }
+    return newArr;
+}
+
+
+
+
+if (sizeArr>0) 
+{
+    string[] array = CreateArray(sizeArr);
+    string[] newArray = FormatArray(array);
+    PrintArray(newArray);
+}
+else Console.WriteLine("Некоректно введено число");
